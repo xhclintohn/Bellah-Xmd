71d17eb5b1fe06175dd8447f9940382dbc7d137b
Tennor-modz <giddysarry@gmail.com>
2024-12-24 10:26:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~SvB0zRKI#lBRkRM3LIx3vrIN-UYI04g-PNvv_amp32Aeeytq7XlA' 
 //owmner v card
 global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
 global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
