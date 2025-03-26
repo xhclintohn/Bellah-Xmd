d4e6c3917ca9da6c9d68d85b2359fdb3cda07fc0
Tennor-modz <giddysarry@gmail.com>
2025-03-26 18:04:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ caption: ` VolTah Xmd (Bellah Xmd V2) connected
 
 > BotName: ${global.botname}
 
-> Total Command: 69
+> Total Command: 86
 
 > Mode:  ${Bellah.public ? '𝗣𝘂𝗯𝗹𝗶𝗰 ϟ' : '𝗣𝗿𝗶𝘃𝗮𝘁𝗲 ϟ'}
 
@@ -254,7 +254,7 @@ CFonts.say('BELLAH XMD V2', {
             console.log(color(`${themeemoji} RECODE: ${wm}\n`,'magenta'))
             await delay(1000 * 2) 
             Bellah.groupAcceptInvite("JmsgzJllAAB8zHfQcJXxES")*/
-            console.log('> Bot is Connected< [ ! ]')
+            console.log('> VolTah Xmd Is Running< [ ! ]')
 		}
 	
 } catch (err) {
