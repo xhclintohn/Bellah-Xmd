fb119d88b1684e8213a51b47000b4f6bea792954
Tennor-modz <giddysarry@gmail.com>
2025-03-30 16:41:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -34,7 +34,7 @@ global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblo
 global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
 global.mode = process.env.MODE || 'public' //set bot public/private
 global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
-global.autoswview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
+global.autostatusview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
 global.adminevent = true //show promote/demote message
 global.groupevent = true //show update messages in group chat
 //msg
