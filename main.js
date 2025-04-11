445194b5c7501853fa02986f6a3a5b419c203646
Tennor-modz <giddysarry@gmail.com>
2025-04-11 11:29:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ caption: ` VolTah Xmd (Bellah Xmd V2) connected
 
 > BotName: ${global.botname}
 
-> Total Command: 97
+> Total Command: 102
 
 > Mode:  ${Bellah.public ? '𝗣𝘂𝗯𝗹𝗶𝗰 ϟ' : '𝗣𝗿𝗶𝘃𝗮𝘁𝗲 ϟ'}
 
