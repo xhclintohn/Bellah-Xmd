e49d8eb039a3816fe5a3c656c44092e5eb00bf31
Tennor-modz <giddysarry@gmail.com>
2025-05-03 20:33:48 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ const antilinkgc = process.env.ANTILINK_GC || 'TRUE';
 global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
 global.autostatusview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
 global.adminevent = true //show promote/demote message
-global.groupevent = process.env.GROUP_EVENT || 'true' //show update messages in group chat
+global.groupevent = process.env.GROUP_EVENT || 'false' //show update messages in group chat
 //msg
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
